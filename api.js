fe76cd45d503d4e46b38d724c33d892b8919d3b9
rbnjathanna <rbnjathanna@gmail.com>
2021-04-08 16:58:27 +0530

    create new API for role

--- api.js
+++ api.js
@@ -223,6 +223,34 @@ app.post('/addEvent',function(req,res){
 	});
 })
 
+app.post('/addRole',function(req,res){
+	var a = new Date(), month = (a.getMonth()+1), mon = '', dte = a.getDate(), dt = '';
+	month < 10 ? mon = "0"+month : mon = month;
+	dte < 10 ? dt = "0"+dte : dt = dte;
+	var reqdte = a.getFullYear()+'-'+mon+'-'+dt+' '+a.getHours()+':'+a.getMinutes()+':'+a.getSeconds();
+
+	// debugger;
+	
+	req.body.created_by_user_id=1;
+	req.body.modified_user_id=1;
+
+	let sql = "INSERT INTO roles (role_name, created_by_user_id, created_date, modified_user_id, modified_user_date) VALUES ('"+req.body.role_name+"','"+req.body.created_by_user_id+"','"+reqdte+"','"+req.body.modified_user_id+"','"+reqdte+"')";
+
+	db.query(sql, function(err, data, fields) {
+		if(err){
+			res.json({
+				status: null,
+				message: err
+		   	});
+		}else{			
+			res.json({
+				status: 200,
+				message: "Role Added successfully."
+			});						
+		}
+	});
+})
+
 app.get('/getEvents',authorize("customer:read"), function(req,res){ //
 	let sql = "SELECT e.event_id, poster_url from events as e inner join event_status as es on e.event_status_id = es.event_status_id where es.event_status in('On Going','Upcoming')";
 

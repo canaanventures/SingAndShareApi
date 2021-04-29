cc6817ff06d1e3da5df4b09957d1bc43e042713e
rbnjathanna <rbnjathanna@gmail.com>
2021-04-29 20:30:25 +0530

    Changes to old one

--- authorization/constant.js
+++ authorization/constant.js
@@ -1,6 +1,6 @@
 module.exports = {
 	//DIR: 'https://vecan.co/uploads/events',
-	BlogDir: '../uploads/blog'
+	BlogDir: '../uploads/blog',
 	redirectlink: 'http://singandshare.vecan.co/register/',
 	info_email: 'reubenjathanna1991@gmail.com',
 	info_password: 'reuben@951991'

0f28224f313e780e2fc45a68d7656d7603ec617a
rbnjathanna <rbnjathanna@gmail.com>
2021-04-29 19:19:35 +0530

    Change the headers to overcome cors error

--- api.js
+++ api.js
@@ -36,13 +36,13 @@ var usertableresp = '';
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://singandshare.vecan.co');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
     // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)

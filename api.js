e0c794f7be8a3c3963e61016d07c8df86c4d27f9
rbnjathanna <rbnjathanna@gmail.com>
2021-06-03 05:43:44 +0530

    Event Register and Mailer

--- api.js
+++ api.js
@@ -148,18 +148,17 @@ const db = mysql.createPool({
 });*/
 
 const mailerdetails = nodemailer.createTransport({
-    //service: 'gmail',
     host: "smtp.gmail.com",
     // host: "smtp.office365.com",
-    port: 587,
-    secure: false, // true for 465, false for other ports
+    port: 465,
+    secure: true, // true for 465, false for other ports
  	auth: {
         user: constant.info_email,
         pass: constant.info_password
-    },
-	tls: {
-	    ciphers: 'SSLv3'
-	}
+    }
+	//tls: {
+	    //ciphers: 'SSLv3'
+	//}
 });
 
 app.post('/login',function(req,res){
